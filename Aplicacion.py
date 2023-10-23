1e0c84743cfdcc6c068b511f8bf8498be2679f7b
Claudio <claudioramon153@gmail.com>
2023-10-23 09:15:09 -0500

    Commit aplicacion

---
+++ Aplicacion.py
@@ -0,0 +1,6 @@
+menu = ("Menu opciones: \n 1. Registrar\n 2. Eliminar\n 3. Editar\n 4. Listar\n 5. Salir")
+
+while True:
+    print("\n" + menu)
+    opcion = input("Ingrese una opcion: ")
+    print("Opcion incorrecta")
