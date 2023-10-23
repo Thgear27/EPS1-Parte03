f958b8eb2f6121ac41fb8271820c2897b18e9505
Daniel Navarro <daniel7navarro7@gmail.com>
2023-10-23 09:44:16 -0500

    Parte 2

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,6 +1,28 @@
+import sqlite3
+conexion = sqlite3.connect("navarro_almacen.db")
+
 menu = ("Menu opciones: \n 1. Registrar\n 2. Eliminar\n 3. Editar\n 4. Listar\n 5. Salir")
+
+# En una cadena guardaremos el script de creacion de la tabla pais
+tabla_producto = """CREATE TABLE producto(
+                    idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+                    codigo INTEGER,
+                    nombre TEXT,
+                    precio INTEGER
+                    )
+            """
+
+
+cursor = conexion.cursor()
+cursor.execute(tabla_producto)
+
 
 while True:
     print("\n" + menu)
     opcion = input("Ingrese una opcion: ")
     print("Opcion incorrecta")
+
+
+
+
+conexion.close()
